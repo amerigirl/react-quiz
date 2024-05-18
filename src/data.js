f44fdbda9402ed61eb23c7ec3f0d228be5a2f6f7
Beasia <amerigirl29@gmail.com>
2024-05-18 12:27:30 -0700

    only 4 questions left :)

--- src/data.js
+++ src/data.js
@@ -1,40 +1,58 @@
 [
-    {
-        question: "What mountain is visible from the West Seattle Bridge on a clear day?",
-        incorrectAnswers: [
-            "Mount Everest",
-            "Broken Top",
-            "Mount Kilimanjaro",
-        ], 
-        correctAnswer: "Mount Rainier"
-    },
+  {
+    question:
+      "What mountain is visible from the West Seattle Bridge on a clear day?",
+    incorrectAnswers: ["Mount Everest", "Broken Top", "Mount Kilimanjaro"],
+    correctAnswer: "Mount Rainier",
+  },
 
-    {
-        question: "There's a troll in Seattle!  He's huge and hides under a bridge in what part of town?",
-        incorrectAnswers: [
-            "Renton",
-            "White Center",
-            "Capitol Hill",
-
-        ], 
-        correctAnswer: "Fremont"
-    },
-    {
-    question: "Seattle has bridges, lol!  But it's famous for having the longest of this type of bridge:",
+  {
+    question:
+      "There's a troll in Seattle!  He's huge and hides under a bridge in what part of town?",
+    incorrectAnswers: ["Renton", "White Center", "Capitol Hill"],
+    correctAnswer: "Fremont",
+  },
+  {
+    question:
+      "Seattle has bridges, lol!  But it's famous for having the longest of this type of bridge:",
     incorrectAnswers: [
-        "The longest double-decker bridge",  
-        "The world's tallest bridge",
-        "The shortest bridge in America",
-      
+      "The longest double-decker bridge in America",
+      "The world's tallest bridge",
+      "The shortest bridge in America",
     ],
-    correctAnswer: "The world's longest floating bridge"
-    },
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
+    correctAnswer: "The world's longest floating bridge",
+  },
 
-]
+  {
+    question:
+      "Seattle has art museums of course, and it's home to this not-so-nicely named one: ",
+    incorrectAnswers: [
+      "The Go-Away Museum",
+      "The Museum of Useless Clothes",
+      "The Mueseum of Bad Drivers",
+    ],
+    correctAnswer: "The Museum of Bad Art",
+  },
+
+  {
+    question:
+      "Despite being one of the rainiest cites in country, Seattlites buy more of this item than any other city in America:",
+    incorrectAnswers: ["Bathing suits", "Snow shoes", "Sunscreen"],
+    correctAnswer: "Sunglasses",
+  },
+
+  {
+    question:
+      "Pickleball, one of the fastest growing sports around the world, began in 1965 on this island near Seattle:",
+    incorrectAnswers: ["Whidbey Island", "San Juan Island", "Vashon Island"],
+    correctAnswer: "Bainbridge Island",
+  },
+
+  {},
+
+  {},
+
+  {},
+
+  {},
+];

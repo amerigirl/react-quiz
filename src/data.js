078afbae2c794b815b8eb5ab2b9500b6b5f279de
Beasia <amerigirl29@gmail.com>
2024-05-18 12:06:52 -0700

    the first couple of quiz questions are complete

---
+++ src/data.js
@@ -0,0 +1,40 @@
+[
+    {
+        question: "What mountain is visible from the West Seattle Bridge on a clear day?",
+        incorrectAnswers: [
+            "Mount Everest",
+            "Broken Top",
+            "Mount Kilimanjaro",
+        ], 
+        correctAnswer: "Mount Rainier"
+    },
+
+    {
+        question: "There's a troll in Seattle!  He's huge and hides under a bridge in what part of town?",
+        incorrectAnswers: [
+            "Renton",
+            "White Center",
+            "Capitol Hill",
+
+        ], 
+        correctAnswer: "Fremont"
+    },
+    {
+    question: "Seattle has bridges, lol!  But it's famous for having the longest of this type of bridge:",
+    incorrectAnswers: [
+        "The longest double-decker bridge",  
+        "The world's tallest bridge",
+        "The shortest bridge in America",
+      
+    ],
+    correctAnswer: "The world's longest floating bridge"
+    },
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+
+]
